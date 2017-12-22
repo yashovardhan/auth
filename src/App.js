5b31c7049d4723532cf4d0a7766ceb2345907684
Yashovardhan Agrawal <yashovardhan.agrawal@gmail.com>
2017-12-22 18:27:43 +0530

    Add firebase authentication

--- src/App.js
+++ src/App.js
@@ -1,8 +1,20 @@
 import React, { Component } from 'react';
 import { View, Text } from 'react-native';
+import firebase from 'firebase';
 import { Header } from './components/common';
 
 class App extends Component {
+  componentWillMount() {
+    firebase.initializeApp({
+    apiKey: 'AIzaSyC7_d5Cn3UDmKsckJjQKyFu8qDv6jt66IM',
+    authDomain: 'auth-f5efa.firebaseapp.com',
+    databaseURL: 'https://auth-f5efa.firebaseio.com',
+    projectId: 'auth-f5efa',
+    storageBucket: 'auth-f5efa.appspot.com',
+    messagingSenderId: '55124786790'
+  });
+  }
+
   render() {
     return (
       <View>
